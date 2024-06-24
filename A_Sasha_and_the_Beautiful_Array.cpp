a0794136dbc4b0eacc54e8e914ea8dfb1d29ffe4
Aman Raj Singh Mourya <amanrajmourya7@gmail.com>
2024-06-24 16:19:08 +0530

    24-06-2024

---
+++ A_Sasha_and_the_Beautiful_Array.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+void solve()
+{
+    // 100 54 80 43 90
+    // 43 54 80 90 100
+    // 100 43 90 54 80
+    //
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        // write code here
+        int n, i = 0;
+        cin >> n;
+        vi a(n);
+        fo(i, n)
+                cin >>
+            a[i];
+        sort(a.begin(), a.end());
+        int ans = 0;
+
+        for (i = 1; i < n; i++)
+        {
+            ans += a[i] - a[i - 1];
+        }
+        cout << ans << endl;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+    return 0;
+}
