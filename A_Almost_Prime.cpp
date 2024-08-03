a324844e2a7b739826b42e6cdcc8d3c62041bf7e
Aman Raj Singh Mourya <amanrajmourya7@gmail.com>
2024-08-03 14:45:22 +0530

    04-08-2024

---
+++ A_Almost_Prime.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+bool primeFactors(int n)
+{
+    set<int> prime;
+
+    while (n % 2 == 0)
+    {
+        prime.insert(2);
+        n = n / 2;
+    }
+
+    for (int i = 3; i <= sqrt(n); i = i + 2)
+    {
+        while (n % i == 0)
+        {
+            prime.insert(i);
+            n = n / i;
+        }
+    }
+
+    if (n > 2)
+    {
+        prime.insert(n);
+    }
+    return prime.size() == 2;
+}
+void solve()
+{
+    int n;
+    cin >> n;
+    int count = 0;
+    while (n > 0)
+    {
+        if (primeFactors(n))
+        {
+            count++;
+        }
+        n--;
+    }
+    cout << count << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+    return 0;
+}
