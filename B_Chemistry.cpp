1a0e07043354b1a6c14a698ecb0f9348b8161215
Aman Raj Singh Mourya <amanrajmourya7@gmail.com>
2024-08-04 19:56:57 +0530

    04-08-2024

---
+++ B_Chemistry.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+void solve()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        string s;
+        cin >> s;
+        map<char, int> m;
+        for (int i = 0; i < n; i++)
+        {
+            m[s[i]]++;
+        }
+        int odd_count = 0;
+        int even = 0;
+        for (auto i : m)
+        {
+            if (i.second % 2 == 1)
+            {
+                odd_count++;
+            }
+            if (i.second == 1)
+            {
+                even++;
+            }
+        }
+        if (odd_count - 1 > k)
+            cout << "NO\n";
+
+        else
+            cout << "YES\n";
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+    return 0;
+}
