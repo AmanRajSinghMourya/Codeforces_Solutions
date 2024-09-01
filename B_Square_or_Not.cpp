43fa74fd6e55f41ddc70d40e4aec65b4ce4f6d16
Aman Raj Singh Mourya <amanrajmourya7@gmail.com>
2024-09-01 23:00:54 +0530

    01-09-2024

---
+++ B_Square_or_Not.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+void solve()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        int n;
+        cin >> n >> s;
+
+        int sqr = floor(sqrt(n));
+        string temp = "";
+        if (sqr * sqr == n)
+        {
+            for (int k = 0; k < sqr; k++)
+            {
+                for (int f = 0; f < sqr; f++)
+                {
+                    if (k == 0 || f == 0 || k == sqr - 1 || f == sqr - 1)
+                    {
+                        temp += "1";
+                    }
+                    else
+                    {
+                        temp += "0";
+                    }
+                }
+            }
+            if (temp == s)
+            {
+                cout << "Yes" << endl;
+            }
+            else
+            {
+                cout << "No" << endl;
+            }
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+    return 0;
+}
