726397dc6f29bc1101c806aa76bdfdb6528c80df
Aman Raj <amanrajmourya7@gmail.com>
2024-11-09 20:35:37 +0530

    09-11-2024

---
+++ A_Two_Screens.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+void solve()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        string s1, s2;
+        cin >> s1 >> s2;
+        int n = s1.size(), m = s2.size();
+
+        int start1 = 0, start2 = 0;
+        int ans = 0;
+        while (start1 < n && start2 < m)
+        {
+            if (s1[start1] == s2[start2])
+                start1++, start2++, ans++;
+            else
+            {
+                break;
+            }
+        }
+        if (ans != 0)
+            ans += 1;
+        ans += (n - start1) + (m - start2);
+        cout << ans << endl;
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+    return 0;
+}
